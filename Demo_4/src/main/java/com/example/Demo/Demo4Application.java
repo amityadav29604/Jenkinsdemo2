60f9032358c6fb41324f2fa7d8d3858fb1bd44ee
amityadav29605 <amityadav29604@gmail.com>
2022-11-11 22:17:57 +0530

    commit

---
+++ Demo_4/src/main/java/com/example/Demo/Demo4Application.java
@@ -0,0 +1,14 @@
+package com.example.Demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo4Application.class, args);
+		System.out.println("Hello");
+	}
+
+}
